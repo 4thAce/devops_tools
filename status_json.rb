789267928e10e28a217bffb0c834eb7e43722455
Rich Magahiz <rich@toutapp.com>
2014-11-13 11:02:29 -0800

    Individual key-value pairs sent to pipe

--- status_json.rb
+++ status_json.rb
@@ -1,23 +1,30 @@
 #!/usr/bin/ruby
 require 'fileutils'
 lines = `mysql -h ec2-54-81-85-214.compute-1.amazonaws.com -u root -e "SHOW GLOBAL STATUS\\G"`.to_a
+#lines = `mysql -h ec2-54-205-251-134.compute-1.amazonaws.com -u root -e "SHOW GLOBAL STATUS\\G"`.to_a
 t=Time.now
 filename="/home/deploy/dbstat/db.#{t.strftime('%Y-%m-%d%H:%M:%S')}.json"
+pipe=open("/var/tmp/logrelay2","w+")   # Nonblocking
 out=File.open(filename,'w')
 output = []
+keyvalue=[]
 lines.each do |line|
         line.chomp!
-	next if /\*\*\*/.match(line)
-	pieces = line.split(':')
-	if pieces[0] == "Variable_name" then
-		output.push('"',"#{pieces[1].lstrip}",'":')
-	elsif /Value/.match(pieces[0]) then
-		output.push('"',"#{pieces[1].lstrip}",'",')
-         end
+		next if /\*\*\*/.match(line)
+		pieces = line.split(':')
+		if pieces[0] == "Variable_name" then
+			keyvalue[0]='"',"prod-#{pieces[1].lstrip}",'":'
+		elsif /Value/.match(pieces[0]) then
+			keyvalue[1]='"',"#{pieces[1].lstrip}",'",'
+    	end
+    	pipe.puts("{#{keyvalue}}")
+    	output.push(keyvalue.join(''),",")
 end
 # Remove extra comma
 output[-1].chop!
 out.puts '{'+output.join('')+ '}'
 out.close
 
-FileUtils.cp(filename,"/var/tmp/logrelay2")
+pipe.flush
+pipe.close
+#FileUtils.cp(filename,"/var/tmp/logrelay2")
--- statusrds.rb
+++ statusrds.rb
@@ -5,17 +5,15 @@ lines = `mysql -h toutapp-proddb.c01ai4mq2ltz.us-east-1.rds.amazonaws.com -u roo
 require 'fileutils'
 t=Time.now
 # Alarm if there is no response
+pipe=open("/var/tmp/logrelay2","w+")   # Nonblocking
 if !File.exists?("/home/deploy/triggered") and lines == '' then
-	curl_command <<EOF
-	curl -H "Content-type: application/json" -X POST \
+	`curl -H "Content-type: application/json" -X POST \
 	-d '{ "service_key": "5c0050adfd9644a49333571e595fd656", \
 	"incident_key": "toutapp-proddb/HTTP", \
 	"event_type": "trigger", "description":"No response from RDS database toutapp-proddb", \
 	"client":"statusrds.rb","client_url":"http://www1.toutapp.com",\
 	"details":{"trigger_time":"#{t.strftime('%Y-%m-%d %H:%M:%S')}"}}' \
-	 >  "https://events.pagerduty.com/generic/2010-04-15/create_event.json"
-	EOF
-	`${curl_command}`
+	 >  "https://events.pagerduty.com/generic/2010-04-15/create_event.json"`
 	trigger=File.open("/home/deploy/triggered",'w')
 	trigger.puts("Alert triggered at #{t.strftime('%Y-%m-%d %H:%M:%S')}")
 	trigger.close
@@ -24,19 +22,25 @@ else
 	filename="/home/deploy/dbstat/db.rds.#{t.strftime('%Y-%m-%d%H:%M:%S')}.json"
 	out=File.open(filename,'w')
 	output = []
+	keyvalue=[]
 	lines.each do |line|
         line.chomp!
 		next if /\*\*\*/.match(line)
 		pieces = line.split(':')
 		if pieces[0] == "Variable_name" then
-			output.push('"',"#{pieces[1].lstrip}",'":')
+			keyvalue[0]='"',"rds-#{pieces[1].lstrip}",'":'
 		elsif /Value/.match(pieces[0]) then
-			output.push('"',"#{pieces[1].lstrip}",'",')
+			keyvalue[1]='"',"#{pieces[1].lstrip}",'",'
     	end
+    	pipe.puts("{#{keyvalue}}")
+    	output.push(keyvalue.join(''),",")
 	end
 	# Remove extra comma
 	output[-1].chop!
 	out.puts '{'+output.join('')+'}'
 	out.close
-	FileUtils.cp(filename,"/var/tmp/logrelay2")
+
+	pipe.flush
+	pipe.close
+	#FileUtils.cp(filename,"/var/tmp/logrelay2")
 end
